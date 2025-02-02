0112365aef5f9f269a06472565ecf27c18774c1a
Anandhu N V <anandhu7299@gmail.com>
2025-02-02 18:22:28 +0000

    title changed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins from Dji
         </a>
       </header>
       <p>
