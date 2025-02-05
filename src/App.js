b5895a3ad338e3c632f18402dbe2fde98713abff
Anandhu N V <anandhu7299@gmail.com>
2025-02-05 18:44:35 +0000

    ut fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins from Udemy
+          Learn Jenkins on Udemy
         </a>
       </header>
       <p>
